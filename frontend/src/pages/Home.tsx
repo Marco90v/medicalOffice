bf8f8399995e30285ebafb28c201b80d7ede8ff7
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2023-09-10 08:13:41 -0400

    feat: ajustes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,26 +9,30 @@ function Home() {
         <div className="m-auto">
             <h1 className="text-center mb-4 text-xl font-bold underline underline-offset-4">Patients in queue for care</h1>
             <table className="m-auto border border-solid border-slate-500 overflow-hidden rounded-md">
-                <tr className="text-white bg-black">
-                    <th className="px-10 py-2">Full Name</th>
-                    <th className="px-10 py-2">Patient care</th>
-                </tr>
-                {
-                    state?.map(({_id, dni, patient})=>{
-                        return (
-                            <tr key={_id} className="text-center border-x-2 odd:b-white even:bg-slate-100 last:border-b-2 hover:bg-slate-200">
-                                <td className="py-2 px-1">{patient}</td>
-                                <td className="py-2 px-1">
-                                    <button type="button" onClick={()=>navigate(`Patient/${dni}`)}
-                                        className="btn bg-blue-500 hover:bg-blue-400 px-2 py-1"
-                                    >
-                                        Attend
-                                    </button>
-                                </td>
-                            </tr>
-                        );
-                    })
-                }
+                <thead>
+                    <tr className="text-white bg-black">
+                        <th className="px-10 py-2">Full Name</th>
+                        <th className="px-10 py-2">Patient care</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        state?.map(({_id, dni, patient})=>{
+                            return (
+                                <tr key={_id} className="text-center border-x-2 odd:b-white even:bg-slate-100 last:border-b-2 hover:bg-slate-200">
+                                    <td className="py-2 px-1">{patient}</td>
+                                    <td className="py-2 px-1">
+                                        <button type="button" onClick={()=>navigate(`Patient/${dni}`)}
+                                            className="btn bg-blue-500 hover:bg-blue-400 px-2 py-1"
+                                        >
+                                            Attend
+                                        </button>
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
             </table>
         </div>
     );
--- frontend/src/pages/Patient.tsx
+++ frontend/src/pages/Patient.tsx
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom";
+import { BASE_URL } from "../utils";
+import { useFetch } from "../hooks/useFetch";
 
 function Patient(){
     const { dni } = useParams();
+    // const { state, getFetch, setFetch, error:errorFetch } = useFetch<queue[]>(BASE_URL, "patient");
+
     console.log(dni);
     return(
         <></>
